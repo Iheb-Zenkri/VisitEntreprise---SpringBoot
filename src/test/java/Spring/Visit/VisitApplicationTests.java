07726a675ac82bcc952a6769bc452264ec434c5d
Iheb Zenkri <zenkriiheb@gmail.com>
2025-02-23 15:38:27 +0100

    first commit : initialize Spring project

---
+++ src/test/java/Spring/Visit/VisitApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.Visit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
