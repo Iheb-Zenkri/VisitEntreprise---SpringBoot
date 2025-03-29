30163fc6d326a432fd2eed494c9f95abae1f70e4
Iheb Zenkri <zenkriiheb@gmail.com>
2025-03-29 02:56:57 +0100

    setting up Visit Program endpoints

---
+++ src/main/java/Spring/Visit/DocumentPackage/entities/VisitProgram.java
@@ -0,0 +1,30 @@
+package Spring.Visit.DocumentPackage.entities;
+
+import Spring.Visit.UserPackage.entities.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "visit_programs")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VisitProgram {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "document_id", nullable = false)
+    private Document document;
+
+    @ManyToOne
+    @JoinColumn(name = "added_by", nullable = false)
+    private User addedBy;
+
+    @Column(name = "visit_id", nullable = false,unique = true)
+    private Long visitId;
+}
